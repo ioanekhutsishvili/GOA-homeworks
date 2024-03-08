bddc94c3265956fab2ab2039d25da40f00b5f8c3
ioanekhutsishvili <ioanekhutsishvili1509@gmail.com>
2024-03-08 13:49:04 +0400

    commit

---
+++ day5/lesson5.py
@@ -0,0 +1,9 @@
+age = int(input("please enter your age: "))
+age1 = int(input("please enter your brothers age: "))
+print(type(age))
+print(type(age1))
+
+
+print(age/age1)
+print(age//age1)
+print(age%age1)
