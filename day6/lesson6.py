04f06a26f4b41d43f028440d626756dbf0ba3faa
ioanekhutsishvili <ioanekhutsishvili1509@gmail.com>
2024-03-15 00:14:05 +0400

    commit

---
+++ day6/lesson6.py
@@ -0,0 +1,84 @@
+num = input("enter number: ")
+num1 = float(num)
+print(num == num1)
+
+
+#>
+print(1 > 2)
+print(2 > 1)
+print(3 > 2)
+print(3 > 1)
+print(2 > 3)
+
+
+#>=
+print(4 >= 3)
+print(3 >= 4)
+print(2 >= 3)
+print(3 >= 2)
+print(1 >= 2)
+
+
+
+#<
+print(5 < 4)
+print(4 < 5)
+print(3 < 5)
+print(4 < 6)
+print(2 < 5)
+
+
+#<=
+print(38 <= 87)
+print(57 <= 54)
+print(79 <= 345)
+print(94 <= 803)
+print(67 <= 90)
+
+
+#==
+print(87 == 49)
+print(665 == 86)
+print(65 == 65)
+print(678 == 87)
+print(545 == 545)
+
+
+#!=
+print(6 != 7)
+print(54 != 8)
+print(3 != 3)
+print(346 != 346)
+print(87 != 67)
+
+#hw N8
+num = input("enter number: ")
+num1 = input("enter another number: ")
+print(num > num1)
+print(num >= num1)
+print(num < num1)
+print(num <= num1)
+print(num == num1)
+print(num != num1)
+
+#hw N9
+num = int(input("enter number: "))
+print(num > num + 10)
+print(num >= num + 10)
+print(num < num + 10)
+print(num <= num + 10)
+print(num == num + 10)
+print(num != num + 10)
+
+
+#hw N10
+num = int(input("enter number: "))
+num1 = float(input("enter another number: "))
+print(num == num1)
+print(num != num1)
+
+
+
+
+
+
