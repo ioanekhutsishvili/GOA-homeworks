eeb15807122dbf0d7b48c62c2614e75b00df6850
ioanekhutsishvili <ioanekhutsishvili1509@gmail.com>
2024-03-19 00:10:08 +0400

    commit

--- day1/lesson 1.5 correct.py
+++ day1/lesson 1.5 correct.py
@@ -7,4 +7,4 @@ weight = 50
 
 
 
-print("my full name is" + " " + surname + " " + "but my friends call me" + nickname + " " + "and I am" + str(age) + "my physical data is: height" + str(height) = "cm" + "and weight" + str(wight) + "kg. " + "I am builder (lego builder)")
+print("my full name is" + " " + surname + " " + "but my friends call me" + nickname + " " + "and I am" + str(age) + "my physical data is: height" + str(height) = "cm" + "and weight" + str(weight) + "kg. " + "I am builder (lego builder)")
--- day2/lesson2.py
+++ day2/lesson2.py
@@ -1,3 +1,3 @@
 print("I met nika gelenidze on mesenger they told me that he was going to be my team leader so I joined him. hes pretty nice guy. helps me when i dont know what to do or how to do GOAs homeworks.")
 
-print("i like goa too well sure its gonna be hard at first sight but i wil get used to it. mentor seems nice too he knows lot about programming. I hope I will be a chad like him :)")
+print("i like goa too well sure its gonna be hard at first sight but i wil get used to it. mentor seems nice too he knows lot about programming. I hope I will be a chad like him")
