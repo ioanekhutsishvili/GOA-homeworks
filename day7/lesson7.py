4bc62e5cc5446699308b1d9eee8406c0e763c9b0
ioanekhutsishvili <ioanekhutsishvili1509@gmail.com>
2024-03-21 21:04:16 +0400

    commit

---
+++ day7/lesson7.py
@@ -0,0 +1,34 @@
+#hw1
+celsius_temp = int(input("please enter temperature in celsius: "))
+
+
+fahrenheite_temp = (celsius_temp * 1.8) + 32
+
+print(fahrenheite_temp)
+
+#hw2
+age = int(input("please enter your age: "))
+
+print(age > 12 and age < 20)
+#hw3
+length = float(input("enter number: "))
+width = float(input("enter number: "))
+area = length * width
+print(area)
+#hw4
+user_number = int(input("enter number: "))
+print(user_number)
+
+print(user_number > 100 or user_number * 18 )
+
+#hw5 and
+print(1 > 100 and 300 < 200)
+print(1 > 100 and 50 < 200)
+print(1 > 0.5 and 300 < 200)
+print(200 > 100 and 100 < 200)
+#or
+print(1 > 100 or 300 < 200)
+print(1 > 100 or 100 < 200)
+print(150 > 100 or 300 < 200)
+print(150 > 100 or 150 < 200)
+
