3b3f22bf2f4f25cc3958c46476111effa560ddd4
ioanekhutsishvili <ioanekhutsishvili1509@gmail.com>
2024-04-11 22:29:35 +0400

    commit

---
+++ day9/lesson9.py
@@ -0,0 +1,7 @@
+num1 = int(input("please enter your salary:"))
+if num1 > 10000:
+    print("გოაში სწავლობდი?")
+if num1 < 10000:
+    print("you mid")
+if num1 < 1000:
+    print("შემოსულიყავი გოაში მატრიცელო")
