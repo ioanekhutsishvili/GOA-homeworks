10df714b2c6aa9413dffe7246bc863417ce6a30d
ioanekhutsishvili <ioanekhutsishvili1509@gmail.com>
2024-04-22 18:56:38 +0400

    commit

---
+++ day01/homework/lesson1.py
@@ -0,0 +1,10 @@
+name = "ioane"
+surname = "khutsishvili"
+nickname = "xuco"
+age = 14
+height = 155
+weight = 50
+
+
+
+print("my full name is" + " " + surname + " " + "but my friends call me" + nickname + " " + "and I am" + str(age) + "my physical data is: height" + str(height) "cm" + "and weight" + str(weight) + "kg. " + "I am builder (lego builder)")
