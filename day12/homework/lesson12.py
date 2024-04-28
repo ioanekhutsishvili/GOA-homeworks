521ac90c2f18d4fd9c855b7541bb2c69c8d9ee3a
ioanekhutsishvili <ioanekhutsishvili1509@gmail.com>
2024-04-28 16:47:25 +0400

    commit

---
+++ day12/homework/lesson12.py
@@ -0,0 +1,23 @@
+for i in range(10):
+    if i % 2 == 1:
+        print(i)
+
+#hw2
+for i in range(50):
+    if i % 5 == 0:
+        print(i)
+
+#hw3
+num = int(input("please enter number: "))
+for i in range(num):
+    print(i)
+
+#hw4
+num = 0
+for i in range(50):
+    num = num + i
+    print(num)
+
+
+
+
